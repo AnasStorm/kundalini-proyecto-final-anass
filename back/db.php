0cdb91402891e1c50f9e909381d25f5aa9c1425c
Anass El Guennouni <anasselguennouni@Anasss-MacBook-Pro.local>
2024-11-22 10:41:43 +0100

    initial

---
+++ back/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db = 'db';  
+$user = 'root';  
+$pass = 'root'; 
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
